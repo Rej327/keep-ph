5beb05db2d70746b92346e5eaae26c59ff96fca7
Rej327 <resujeff27@gmail.com>
2025-12-01 12:41:30 +0800

    chore: use mailroomlayout

--- src/app/(protected)/(customer)/mailroom/page.tsx
+++ src/app/(protected)/(customer)/mailroom/page.tsx
@@ -1,6 +1,6 @@
-import MailroomClient from "@/components/page/Mailroom/MailroomClient";
+import MailroomLayout from "@/components/page/MailroomPage/MailroomLayout";
 import React from "react";
 
 export default function page() {
-  return <MailroomClient />;
+  return <MailroomLayout />;
 }
